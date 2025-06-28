8758a393be274a5ec15413d1d382e0e521fbfe9e
Jrpdm05 <johnrubenmombouli@gmail.com>
2025-06-27 21:26:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ For the girl who stole my heart,
 
 Today is your day, and I hope this small world I made brings a smile to your beautiful face my love :).
 Today is a special for you my love, as you go into adulthood and slowly grow out of your teenage years.
-These past few months have been the best months in my life babygirl. With you in my life, I have been the happiest man in the world.
-Together, we smiled, we hugged, we joked, and we've had the time of our lives. I'm happy and blessed to have you in my life and I wish you a happy birthday! 
+These past few months with have been the best months in my life babygirl. With you in my life, I am  the happiest man in the world.
+Together, we smile, we hug each other, we joke around and we've had the time of our lives. I'm happy and blessed to have you in my life and I wish you a happy birthday! 
 Although it's not much, I hope this letter of my affection and love goes straight to your heart.
 
 
